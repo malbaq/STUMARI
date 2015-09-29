2ab3f427b1e07c3e6fbe032235b602b393db162e
Tom Malary <tom@Toms-MacBook-Air.local>
2015-09-29 21:31:19 +0300

    PFQueryTableVC implemented

---
+++ STUMARI/TableOfRentalsPFQueryTableViewController.swift
@@ -0,0 +1,83 @@
+//
+//  TableOfRentalsPFQueryTableViewController.swift
+//  STUMARI
+//
+//  Created by Tom Malary on 9/29/15.
+//  Copyright (c) 2015 YOUTIR LLC. All rights reserved.
+//
+
+import UIKit
+
+class TableOfRentalsPFQueryTableViewController: PFQueryTableViewController {
+    
+    override init(style: UITableViewStyle, className: String?) {
+        super.init(style: style, className: className)
+        parseClassName = "Rental"
+        pullToRefreshEnabled = true
+        paginationEnabled = true
+        objectsPerPage = 25
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        parseClassName = "Rental"
+        pullToRefreshEnabled = true
+        paginationEnabled = true
+        objectsPerPage = 25
+    }
+    
+    override func queryForTable() -> PFQuery {
+        let query = PFQuery(className: self.parseClassName!)
+        
+        // If no objects are loaded in memory, we look to the cache first to fill the table
+        // and then subsequently do a query against the network.
+        if self.objects!.count == 0 {
+            query.cachePolicy = .CacheThenNetwork
+        }
+        
+        query.orderByDescending("createdAt")
+        
+        return query
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath, object: PFObject?) -> PFTableViewCell? {
+        let cellIdentifier = "cell"
+        
+        var cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as? PFTableViewCell
+        if cell == nil {
+            cell = PFTableViewCell(style: .Subtitle, reuseIdentifier: cellIdentifier)
+        }
+        
+        // Configure the cell to show todo item with a priority at the bottom
+        if let object = object {
+            cell!.textLabel?.text = object.objectForKey("test") as? String
+            //let priority = object.objectForKey("priority") as? String
+            //cell!.detailTextLabel?.text = "Priority \(priority)"
+        }
+        
+        return cell
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ STUMARI/TableOfRentalsPFTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  TableOfRentalsPFTableViewCell.swift
+//  STUMARI
+//
+//  Created by Tom Malary on 9/29/15.
+//  Copyright (c) 2015 YOUTIR LLC. All rights reserved.
+//
+
+import UIKit
+
+class TableOfRentalsPFTableViewCell: PFTableViewCell {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
